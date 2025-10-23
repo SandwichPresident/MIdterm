838a12b02c2ddb6a1d502e5421b02b356562ae9d
SandwichPresident <shb1713@rit.edu>
2025-10-22 23:36:48 -0400

    Question 87

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentNames = ["Alex", "Sam", "Lee", "Nick"];
+            foreach (var name in studentNames)
+            {
+                Console.WriteLine("Welcome " + name + "!");
+                if (name == "Nick")
+                {
+                                       Console.WriteLine("Wow! You have the same name as the instructor!");
+                }
+            }
+        }
+    }
+}
